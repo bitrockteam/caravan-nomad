f735c35913aa61a69933de4291aa344eb1211346
efbar <17676301+efbar@users.noreply.github.com>
2020-07-17 21:39:07 +0200

    align

--- modules/nomad-cluster/main.tf
+++ modules/nomad-cluster/main.tf
@@ -5,23 +5,23 @@ resource "null_resource" "nomad_cluster_node_deploy_config" {
   
   for_each = var.cluster_nodes
 
- provisioner "file" {
-    destination = "/tmp/nomad.hcl"
-    content = <<-EOT
-    ${templatefile(
-    "${path.module}/nomad-server.hcl",
-    {
-      cluster_nodes = var.cluster_nodes
-    }
-)}
-    EOT
-connection {
-  type        = "ssh"
-  user        = var.ssh_user
-  private_key = var.ssh_private_key
-  timeout     = var.ssh_timeout
-  host        = var.cluster_nodes_public_ips != null ? var.cluster_nodes_public_ips[each.key] : each.value
-}
+  provisioner "file" {
+      destination = "/tmp/nomad.hcl"
+      content = <<-EOT
+      ${templatefile(
+      "${path.module}/nomad-server.hcl",
+      {
+        cluster_nodes = var.cluster_nodes
+      }
+  )}
+      EOT
+  connection {
+    type        = "ssh"
+    user        = var.ssh_user
+    private_key = var.ssh_private_key
+    timeout     = var.ssh_timeout
+    host        = var.cluster_nodes_public_ips != null ? var.cluster_nodes_public_ips[each.key] : each.value
+  }
 }
 provisioner "remote-exec" {
   inline = ["sudo mv /tmp/nomad.hcl /etc/nomad.d/nomad.hcl && sudo mv /tmp/nomad.hcl.tmpl /etc/nomad.d/nomad.hcl.tmpl"]
