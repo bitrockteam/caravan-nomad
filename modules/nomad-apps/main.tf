c6f096e1e82fa98a025403fa31d3fc343b149359
efbar <17676301+efbar@users.noreply.github.com>
2020-09-17 18:37:10 +0200

    remove useless module

--- modules/nomad-apps/main.tf
+++
@@ -1,8 +0,0 @@
-provider "nomad" {
-  address = "http://${var.nomad_endpoint}:4646"
-  region  = "us-east-2"
-}
-
-resource "nomad_job" "jaeger" {
-  jobspec = file("${path.module}/files/jaeger.hcl")
-}
