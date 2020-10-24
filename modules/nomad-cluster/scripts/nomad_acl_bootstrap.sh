ff85988651078ff16272bf834c67a4ca37a3f852
efbar <17676301+efbar@users.noreply.github.com>
2020-10-24 12:05:59 +0200

    add acl bootstrap

---
+++ modules/nomad-cluster/scripts/nomad_acl_bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+nomad acl bootstrap
+awk '(/Secret/ || /Accessor/)'| sudo tee /root/nomad_tokens && \
+sleep 5s
+export `sudo sh /root/vault.vars` && \
+sudo tee nomad-anon.hcl <<EOT
+namespace "*" {
+  policy       = "write"
+  capabilities = ["alloc-node-exec"]
+}
+
+agent {
+  policy = "write"
+}
+
+operator {
+  policy = "write"
+}
+
+quota {
+  policy = "write"
+}
+
+node {
+  policy = "write"
+}
+
+host_volume "*" {
+  policy = "write"
+}
+EOT
+export NOMAD_TOKEN="`sudo cat /root/nomad_tokens | awk '/Secret/{print $2}'`" && \
+nomad acl policy apply -description "Anonymous policy (full-access)" anonymous nomad-anon.hcl && \
+{ [ -z "`sudo cat /root/nomad_tokens | awk '/Secret/{print $2}'`" ] ||
+  vault kv put secret/nomad/bootstrap_token secretid="`sudo cat /root/nomad_tokens | awk '/Secret/{print $2}'`" accessorid="`sudo cat /root/nomad_tokens | awk '/Access/{print $2}'`"
+} && \
+sudo rm -f /root/nomad_tokens nomad-anon.hcl
