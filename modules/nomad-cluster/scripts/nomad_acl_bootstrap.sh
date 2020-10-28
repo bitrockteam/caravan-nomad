50f4f87395613805717893779efdb8d29993e10c
efbar <17676301+efbar@users.noreply.github.com>
2020-10-28 11:38:27 +0100

    remove anon policy

--- modules/nomad-cluster/scripts/nomad_acl_bootstrap.sh
+++ modules/nomad-cluster/scripts/nomad_acl_bootstrap.sh
@@ -7,34 +7,7 @@ done
 nomad acl bootstrap | awk '(/Secret/ || /Accessor/)'| sudo tee /root/nomad_tokens && \
 sleep 5s
 export `sudo sh /root/vault.vars` && \
-sudo tee nomad-anon.hcl <<EOT
-namespace "*" {
-  policy       = "write"
-  capabilities = ["alloc-node-exec"]
-}
-
-agent {
-  policy = "write"
-}
-
-operator {
-  policy = "write"
-}
-
-quota {
-  policy = "write"
-}
-
-node {
-  policy = "write"
-}
-
-host_volume "*" {
-  policy = "write"
-}
-EOT
 export NOMAD_TOKEN="`sudo cat /root/nomad_tokens | awk '/Secret/{print $4}'`" && \
-nomad acl policy apply -description "Anonymous policy (full-access)" anonymous nomad-anon.hcl && \
 { [ -z "`sudo cat /root/nomad_tokens | awk '/Secret/{print $2}'`" ] ||
   vault kv put secret/nomad/bootstrap_token secretid="`sudo cat /root/nomad_tokens | awk '/Secret/{print $4}'`" accessorid="`sudo cat /root/nomad_tokens | awk '/Access/{print $4}'`" && \
   vault write nomad/config/access address=http://127.0.0.1:4646 token="`sudo cat /root/nomad_tokens | awk '/Secret/{print $4}'`" && \
