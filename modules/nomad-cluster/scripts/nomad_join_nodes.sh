58ff9719ca0f5fa177ef4d507acf60e57ac17508
efbar <17676301+efbar@users.noreply.github.com>
2021-02-11 17:49:13 +0100

    "better" join

--- modules/nomad-cluster/scripts/nomad_join_nodes.sh
+++ modules/nomad-cluster/scripts/nomad_join_nodes.sh
@@ -12,12 +12,16 @@ echo "Is service up?"
 while ! curl --output /dev/null --silent --fail  http://localhost:4646; do 
   sleep 5s
 done
-echo "yes.."
+echo "Yes."
 
-echo "Join nodes"
+echo "Join nodes.."
 for n in $${NODES[@]}; do
   if [[ "$n" != "$HOST" ]];
   then
+    while ! curl --output /dev/null --silent --fail  http://$n:4646; do 
+      sleep 5s
+    done
     /usr/local/bin/nomad server join $n
+    echo "Node $n joined."
   fi
 done
